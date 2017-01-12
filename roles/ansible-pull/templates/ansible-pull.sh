7285ddb169ba89e0881a5601c4f11ccebc8cae25
Finn <finn@finn.io>
2017-01-11 17:45:18 -0800

    Only run if changed

--- roles/ansible-pull/templates/ansible-pull.sh
+++ roles/ansible-pull/templates/ansible-pull.sh
@@ -2,7 +2,7 @@
 # Sometimes the $PATH gets messed up in cron, so lets start by setting the record straight
 if [ "$1" == "nodisown" ]; then
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-  /usr/local/bin/ansible-pull -U https://github.com/thefinn93/ansible-basicshit -C master > /var/log/ansible.log
+  /usr/local/bin/ansible-pull -o -U https://github.com/thefinn93/ansible-basicshit -C master > /var/log/ansible.log
   if [[ "$?" != "0" ]]; then
     link=$(cat /var/log/ansible.log | curl -F 'sprunge=<-' http://sprunge.us)
     ansible localhost -m irc -a "channel=#thefinn93 msg=\"[{{ ansible_fqdn }}] Ansible failed! Full log at $link\" server=irc.freenode.net port=6697 use_ssl=yes nick={{ ansible_hostname }}"
