fa06ddc4b89bbda94fe5d4687e813ca6e2443e0b
Finn <finn@finn.io>
2017-01-11 17:50:20 -0800

    Move comment about PATH setting to right above where we set it

--- roles/ansible-pull/templates/ansible-pull.sh
+++ roles/ansible-pull/templates/ansible-pull.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Sometimes the $PATH gets messed up in cron, so lets start by setting the record straight
 post_to_irc () {
   ansible localhost -m irc -a "channel=#thefinn93 msg=\"[{{ ansible_fqdn }}] $1\" server=irc.freenode.net port=6697 use_ssl=yes nick={{ ansible_hostname }}"
 }
+
 if [ "$1" == "nodisown" ]; then
+  # Sometimes the $PATH gets messed up in cron, so lets start by setting the record straight
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
   /usr/local/bin/ansible-pull -o -U https://github.com/thefinn93/ansible-basicshit -C master > /var/log/ansible.log
   if [[ "$?" != "0" ]]; then
