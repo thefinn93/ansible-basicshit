8edf0ba29deb5c8b7909a523c8e1fd88f0a7e7e6
Finn <finn@finn.io>
2017-01-11 17:48:04 -0800

    Post to IRC for pass and failure

--- roles/ansible-pull/templates/ansible-pull.sh
+++ roles/ansible-pull/templates/ansible-pull.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Sometimes the $PATH gets messed up in cron, so lets start by setting the record straight
+post_to_irc () {
+  ansible localhost -m irc -a "channel=#thefinn93 msg=\"[{{ ansible_fqdn }}] $1\" server=irc.freenode.net port=6697 use_ssl=yes nick={{ ansible_hostname }}"
+}
 if [ "$1" == "nodisown" ]; then
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
   /usr/local/bin/ansible-pull -o -U https://github.com/thefinn93/ansible-basicshit -C master > /var/log/ansible.log
   if [[ "$?" != "0" ]]; then
     link=$(cat /var/log/ansible.log | curl -F 'sprunge=<-' http://sprunge.us)
-    ansible localhost -m irc -a "channel=#thefinn93 msg=\"[{{ ansible_fqdn }}] Ansible failed! Full log at $link\" server=irc.freenode.net port=6697 use_ssl=yes nick={{ ansible_hostname }}"
+    post_to_irc "Ansible failed! Full log at $link"
+  else
+    post_to_irc "Ansible succeeded"
   fi
 else
   $0 nodisown &!
