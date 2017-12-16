d48fc5efb6917af1b0cf8e40795f90d60cf0752b
Finn <finn@finn.io>
2017-12-16 14:52:51 -0800

    Log stderr as well as stdout

--- roles/ansible-pull/templates/ansible-pull.sh
+++ roles/ansible-pull/templates/ansible-pull.sh
@@ -6,10 +6,11 @@ post_to_irc () {
 if [ "$1" == "nodisown" ]; then
   # Sometimes the $PATH gets messed up in cron, so lets start by setting the record straight
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-  /usr/local/bin/ansible-pull -o -U https://git.callpipe.com/finn/ansible-basicshit.git -s 600 -C master> /var/log/ansible.log
-  if [[ "$?" != "0" ]]; then
+  /usr/local/bin/ansible-pull -o -U https://git.callpipe.com/finn/ansible-basicshit.git -s 600 -C master &> /var/log/ansible.log
+  rc = "$?"
+  if [[ "$rc" != "0" ]]; then
     link=$(cat /var/log/ansible.log | curl -F 'sprunge=<-' http://sprunge.us)
-    post_to_irc "Ansible failed! Full log at $link"
+    post_to_irc "Ansible failed (rc ${rc})! Full log at $link"
   fi
 else
   $0 nodisown & disown
