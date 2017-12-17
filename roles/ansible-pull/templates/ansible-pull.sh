2082c128d4888df3be689a56c7f4099955b5ead8
Finn Herzfeld <finn@finn.io>
2017-12-17 11:39:17 -0800

    derp

--- roles/ansible-pull/templates/ansible-pull.sh
+++ roles/ansible-pull/templates/ansible-pull.sh
@@ -8,7 +8,7 @@ if [ "$1" == "nodisown" ]; then
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
   virtualenv --system-site-packages -p python2 /usr/ansible
   /usr/ansible/bin/pip install ansible==2.4.2.0
-  $ANSIBLE_PULL="/usr/ansible/bin/ansible-pull"
+  ANSIBLE_PULL="/usr/ansible/bin/ansible-pull"
   if [ ! -x $ANSIBLE_PULL ]; then
     ANSIBLE_PULL=$(which ansible-pull)
   fi
@@ -16,7 +16,7 @@ if [ "$1" == "nodisown" ]; then
   rc="$?"
   if [[ "$rc" != "0" ]]; then
     link=$(cat /var/log/ansible.log | nc termbin.com 9999)
-    curl -X POST --data-urlencode "payload={\"username\": \"{{ ansible_fqdn }}\", \"text\": \"Ansible Pull exited with status `${rc}`. <${link}|Full log>\"}" https://hooks.slack.com/services/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
+    curl -X POST --data-urlencode "payload={\"username\": \"{{ ansible_fqdn }}\", \"text\": \"Ansible Pull exited with status \`${rc}\`. <${link}|Full log>\"}" https://hooks.slack.com/services/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
     post_to_irc "Ansible failed (rc ${rc})! Full log at $link"
   fi
 else
