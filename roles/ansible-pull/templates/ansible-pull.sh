3bafc816050bfe97b327eddea55c90b1cc4a324e
Finn <finn@finn.io>
2018-01-09 22:18:22 -0800

    Proper use of variable assignment

--- roles/ansible-pull/templates/ansible-pull.sh
+++ roles/ansible-pull/templates/ansible-pull.sh
@@ -7,7 +7,7 @@ if [ "$1" == "nodisown" ]; then
   # Sometimes the $PATH gets messed up in cron, so lets start by setting the record straight
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
   /usr/local/bin/ansible-pull -o -U https://git.callpipe.com/finn/ansible-basicshit.git -s 600 -C master &> /var/log/ansible.log
-  rc = "$?"
+  rc="$?"
   if [[ "$rc" != "0" ]]; then
     link=$(cat /var/log/ansible.log | curl -F 'sprunge=<-' http://sprunge.us)
     post_to_irc "Ansible failed (rc ${rc})! Full log at $link"
